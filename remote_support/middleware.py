f3529b7e0fdacb5e6eeee3493703b620606dd6b4
MrCreepTon <53111789+MrCreepTon@users.noreply.github.com>
2024-10-24 15:24:22 +0300

    cors bypass for frontend

---
+++ remote_support/middleware.py
@@ -0,0 +1,8 @@
+class CORSMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        return response
